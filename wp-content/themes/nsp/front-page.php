db7d2c0d791df7c38237991d81261b18852e2d15
Nais Alcaraz <nsalcaraz@gmail.com>
2019-08-08 14:19:50 +0200

    Work in progress

--- wp-content/themes/nsp/front-page.php
+++ wp-content/themes/nsp/front-page.php
@@ -1,5 +1,68 @@
 <?php get_header(); ?>
 <main id="content">
-  <h1>Page d'accueil</h1>
+	<div class="nsp-home-presentation">
+		<div class="nsp-home-presentation-logo">
+			<img src= "<?php echo get_stylesheet_directory_uri(); ?>/img/nsp/logo.png"/>
+		</div>
+		<div class="nsp-home-presentation-texte"> 
+			<div class="f-light f-primary">
+				<span class="f-bolder">12—17·11·2019</span>
+				<p class="f-light f-primary">LE RENDEZ-VOUS <br>DE LA BIÈRE ARTISANALE <br> NANTAIS</p>
+			</div>
+			<div>
+				<p class="f-light f-primary">LE FINAL AU SOLILAB <br>le 16 et 17·11·2019</p>
+			</div>
+		</div>
+
+		<div class="nsp-home-presentation-asso">
+			<h2 class="f-light f-white">Qui sommes nous ? </h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+		</div>
+		<div class="nsp-home-presentation-participants">
+			<div class="nsp-home-presentation-participants-header">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nsp/groupeverres.png">
+				<a href="" class="btn btn-pink">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nsp/arrow-pink.svg">
+				</a>
+			</div>
+			<div class="nsp-home-presentation-participants-texte">
+				<h2 class="f-secondary f-light">Les participants</h2>
+				<div class="nsp-home-presentation-participants-item">
+					<p>Brasseries</p>
+					<p>Distributeurs</p>
+					<p>Foodtruck</p>
+					<p>Autres</p>
+					<a href="<php echo get_the_permalink(13); ?>" class="btn btn-blue">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nsp/arrow-blue.svg">
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="nsp-home-programme">
+		<div class="nsp-home-programme-fond">
+			<h2 class="f-primary f-light">Le programme</h2>
+			<div class="flex flex-j-sb nsp-home-programme-fond-block">
+				<div class="nsp-home-programme-fond-block-description">
+					<h3 class="f-primary f-light">La pression</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+				</div>
+				<div class="nsp-home-programme-fond-block-jours">
+					<div class="blue">Mardi</div>
+					<div class="blue">Mercredi</div>
+					<div class="blue">Jeudi</div>
+					<div class="blue">Vendredi</div>
+				</div>
+				<a href="" class="btn btn-blue">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nsp/arrow-blue.svg">					
+				</a>
+			</div>
+		</div>
+	</div>
 </main>
 <?php get_footer(); ?>
--- wp-content/themes/nsp/header.php
+++ wp-content/themes/nsp/header.php
@@ -4,20 +4,29 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width" />
-  <?php wp_head(); ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class('js-fixed-header'); ?>>
   <div id="main-wrapper" class="hfeed">
     <header id="header" class="header js-header-container">
       <div class="container header__wrapper">
-        <div id="branding" class="header__logo">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home" aria-label="Retour à l'accueil">NSP</a>
-        </div>
-        <nav id="menu" class="header__menu js-menu">
+<!--         <div id="branding" class="header__logo">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home" aria-label="Retour à l'accueil"></a>
+        </div> -->
+<!--         <nav id="menu" class="header__menu js-menu">
           <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-        </nav>
+        </nav> -->
       </div>
     </header>
-    <div id="content-wrapper" class="page-container">
+    <div class="wrapper">
+      <nav id="menu" class="active">
+        <div class="menu-title">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" aria-label="Retour à l'accueil"><h1>NANTES SOUS PRESSION</h1></a>
+        </div>
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+      </nav>
