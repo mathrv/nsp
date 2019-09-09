ebae1480e12ab0c4b83856d1218c260a2e7f07bb
Nais Alcaraz <nsalcaraz@gmail.com>
2019-09-09 23:11:03 +0200

    Remove 2nd contact form from the page page-contact.php

--- wp-content/themes/nsp/page-contact.php
+++ wp-content/themes/nsp/page-contact.php
@@ -5,9 +5,6 @@
     <div class="nsp-background-logo">
       <h1 class="f-primary f-light"><?php the_title(); ?></h1>
       <div class="nsp-background-content">
-        <div class="wysiwyg">
-          <?php the_content(); ?>
-        </div>
         <?php echo do_shortcode('[gravityform id="1" title="false" description="false"]'); ?>
       </div>
     </div>
