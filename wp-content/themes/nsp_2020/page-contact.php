3037ee607f9a48047a6bf15e77e9fa7812d91d73
Nais Alcaraz Monteyrol <nsalcaraz@gmail.com>
2020-08-02 23:56:18 +0200

    [contact] mise en place du background

--- wp-content/themes/nsp_2020/page-contact.php
+++ wp-content/themes/nsp_2020/page-contact.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<main id="content">
+<main id="content" class="contact-page">
   <div class="nsp-background">
-      <h1 class="page-title">Nous contacter</h1>
+      <h1 class="page-title contact-title">Nous contacter</h1>
       <?php echo do_shortcode('[gravityform id="1" title="false" description="false"]'); ?>
   </div>
 
