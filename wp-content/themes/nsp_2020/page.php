02ec3e7be8aa257e6bfc2a6938c4f492e0df5951
Nais Alcaraz Monteyrol <nsalcaraz@gmail.com>
2020-08-02 23:26:03 +0200

    [Contact] intégration du formulaire de contact

--- wp-content/themes/nsp_2020/page.php
+++ wp-content/themes/nsp_2020/page.php
@@ -2,14 +2,12 @@
 
 <main id="content">
   <div class="nsp-background">
-    <div class="nsp-background-logo">
       <h1 class="page-title"><?php the_title(); ?></h1>
       <div class="nsp-background-content">
         <?php 
           the_content()
         ?>
       </div>
-    </div>
   </div>
 
 </main>
