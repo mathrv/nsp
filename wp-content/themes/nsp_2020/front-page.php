132c485fb878093b9219a15a232fb9eb6f6a16b7
Thibaut LE LEVIER <thibaut@lelevier.com>
2020-08-19 01:36:26 +0200

    [placeholders] placeholders on home page and delete unused page

--- wp-content/themes/nsp_2020/front-page.php
+++ wp-content/themes/nsp_2020/front-page.php
@@ -9,12 +9,12 @@
 					<p class="bold">Lun. 2 / dim. 8 novembre 2020</p>
 					<p>Final au Solilab les 7 et 8 novembre</p>
 					<p>Suivez-nous sur <a href="https://www.facebook.com/NantesSousPression/" target="_blank">Facebook</a>, <a href="https://twitter.com/nantesspression" target="_blank">Twitter</a> et <a href="https://www.instagram.com/nantessouspression/" target="_blank">Instagram</a> <br> pour les dernières mises à jour.</p>
-					<?php include('partial_booking_button.php') ?>
+<?php include('partial_booking_button.php') ?>
 				</div>
 			</div>
 			<div class="breadcrump"></div>
 			<div class="section section-two marginTopStandard">
-				<?= the_content(); ?>
+<?= the_content(); ?>
 			</div>
 			<div class="breadcrump"></div>
 			<div class="section section-third">
@@ -25,22 +25,30 @@
 <?php
 	$evenements = array('post_type' => 'events', 'posts_per_page' => 6, 'orderby' => 'rand');
 	$evenements_loop = new WP_Query($evenements);
-	while ( $evenements_loop->have_posts() ) {
-		$evenements_loop->the_post();
-		$fields = get_fields(get_the_ID());
+
+	if ($evenements_loop->have_posts()) {
+		while ( $evenements_loop->have_posts() ) {
+			$evenements_loop->the_post();
+			$fields = get_fields(get_the_ID());
 ?>
 						<div class="list-events-element">
 							<h5><?= get_the_title() ?></h5>
 							<?= $fields['event_start_hour'] ?> - <?= $fields['event_place'] ?>
 						</div>
 <?php
-	}
+		}// end while
 ?>
 					</div>
 				</div>
 					
-				<a href="<?= get_page_link(13); ?>" class="see-more">Voir tous les événements <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/see-more.png"></a>
-				<?php include('partial_booking_button.php') ?>
+<a href="<?= get_page_link(13); ?>" class="see-more">Voir tous les événements <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/see-more.png"></a>
+<?php 
+		include('partial_booking_button.php');
+	}
+	else {
+		include('partial_placeholder.php');
+	}
+?>
 			</div>
 
 			<div class="breadcrump"></div>
@@ -48,44 +56,47 @@
 			<div class="section section-four">
 				<h3 class="marginTopStandard">Les brasseries</h3>
 				<div class="list-participants">
-				<?php 
-					$args = array( 'category_id' => 0,
-													'post_type' => 'participants',
-											'orderby' => 'rand',
-											'post_per_page' => 6);
-					$participants_query = new WP_Query( $args );
+<?php 
+	$args = array( 'category_id' => 0,
+									'post_type' => 'participants',
+							'orderby' => 'rand',
+							'post_per_page' => 6);
+	$participants_query = new WP_Query($args);
 
-					while ( $participants_query->have_posts() ) {
-						$participants_query->the_post(); 
-						$participant=get_fields(get_the_ID());
-					?>
+	if ($participants_query->have_posts()) {
+		while ($participants_query->have_posts()) {
+			$participants_query->the_post(); 
+			$participant=get_fields(get_the_ID());
+?>
 					<div class="list-participants-element">
-						<?php 
-							if ( has_post_thumbnail() ) {
-								echo '<p class="list-participants-element-logo">';
-								the_post_thumbnail("small");
-								echo '</p>';
-							}
-						?>
+						<p class="list-participants-element-logo"><?= the_post_thumbnail("small"); ?></p>
 						<div class="nsp-list-element-content">
-							<h5><?php the_title(); ?></h5>
-							<?php if(isset($participant['attendee_country']) && !empty($participant['attendee_country'])): ?>
-								<?= $participant['attendee_country']; ?>
-							<?php endif; ?>
-						</div>
+<h5><?php the_title(); ?></h5>
+<?php if(isset($participant['attendee_country']) && !empty($participant['attendee_country'])): ?>
+	<?= $participant['attendee_country']; ?>
+<?php endif; ?>
+			</div>
 					</div>
-					<?php } ?>
-					<a href="<?= get_page_link(15); ?>" class="see-more">Voir tous les participants <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/see-more.png"></a>
-					<?php include('partial_booking_button.php') ?>
+<?php 
+	} //end while
+?>
+<a href="<?= get_page_link(15); ?>" class="see-more">Voir tous les participants <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/see-more.png"></a>
+<?php 
+		include('partial_booking_button.php');
+	}
+	else {
+		include('partial_placeholder.php');
+	}
+?>
 				</div>
 			</div>
 			<div class="breadcrump"></div>
 			<div class="section section-five">
 				<h3 class="marginTopStandard">Les partenaires</h3>
-				<?php 
-					$Partenairescontent = apply_filters('the_content', get_post_field('post_content', 130));
-					echo $Partenairescontent;
-				?>
+<?php 
+	$Partenairescontent = apply_filters('the_content', get_post_field('post_content', 130));
+	echo $Partenairescontent;
+?>
 			</div>
 		</div>
 	</div>
